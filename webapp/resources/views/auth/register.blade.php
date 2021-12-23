eea44eb613795d358ef40ba782ed87573a80e26f
frenato00 <up201908044@up.pt>
2021-12-23 12:55:13 +0000

    register done

--- webapp/resources/views/auth/register.blade.php
+++ webapp/resources/views/auth/register.blade.php
@@ -1,7 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('register') }}">
+<section class="vh-100 bg-image" style="background: rgb(0,0,0); background: rgba(0,0,0,0.4);">
+  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div class="card" style="border-radius: 15px;">
+            <div class="card-body p-5">
+              <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+              <form>
+
+                <div class="form-outline mb-2">
+                  <input type="text" id="formName" class="form-control form-control-lg" placeholder="Your Name *"/>
+                </div>
+
+                <div class="form-outline mb-2">
+                  <input type="email" id="formUsername" class="form-control form-control-lg" placeholder="Username *"/>
+                </div>
+
+                <div class="form-outline mb-2">
+                  <input type="password" id="formPassword" class="form-control form-control-lg" placeholder="Password *"/>
+                </div>
+
+                <div class="form-outline mb-2">
+                  <input type="password" id="formRPassword" class="form-control form-control-lg" placeholder="Confirm Password *"/>
+                </div>
+
+                <div class="form-outline mb-2">
+                  <input type="email" id="formEmail" class="form-control form-control-lg" placeholder="Email"/>
+                </div>
+
+                <div class="form-outline mb-2">
+                  <input type="tel" id="formPhoneNumber" class="form-control form-control-lg" placeholder="Phone Number"/>
+                </div>
+
+                <div class="form-outline datepicker w-100">
+                    <input type="text" class="form-control form-control-lg" id="birthdayDate" placeholder="Birthday Date"/>
+                </div>
+
+                <div class="form-check d-flex justify-content-center mb-5">
+                  <!-- <input
+                    class="form-check-input me-2"
+                    type="checkbox"
+                    value=""
+                    id="form2Example3cg"
+                  />
+                  <label class="form-check-label" for="form2Example3g">
+                    I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
+                  </label> -->
+                </div>
+
+                <div class="d-flex justify-content-center">
+                  <button type="button" class="btn btn btn-outline-secondary btn-block btn-lg bg-dark text-white">Register</button>
+                </div>
+
+                <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" class="fw-bold text-body"><u>Login here</u></a></p>
+
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- <form method="POST" action="{{ route('register') }}">
     {{ csrf_field() }}
 
     <label for="name">Name</label>
@@ -35,5 +101,5 @@
       Register
     </button>
     <a class="button button-outline" href="{{ route('login') }}">Login</a>
-</form>
+</form> -->
 @endsection
