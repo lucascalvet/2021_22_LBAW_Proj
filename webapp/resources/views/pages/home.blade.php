ff1eb06b787c8a0ae80134617f1a46a6fe797222
José Pedro Ferreira <up201904515@fe.up.pt>
2021-12-28 02:38:56 +0000

    Almost responsive Home View

--- webapp/resources/views/pages/home.blade.php
+++ webapp/resources/views/pages/home.blade.php
@@ -9,33 +9,46 @@ $icon_size = 'fs-3';
 @include('partials.navbar')
 
 @section('content')
-<div class="container vh-100 text-white" style="background-color: rgb(112, 2, 237);">
+<div class="container vw-100 text-white" style="background-color: rgb(112, 2, 237);">
     <div class="row align-self-center">
-        <div class="col-2 d-sm-block d-md-block d-lg-none align-self-center">
-        <div class="row justify-content-center mb-3">
-                <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
-                    <i class="bi bi-bar-chart {{ $icon_size }}"></i>
-                </button>
-            </div>
-            <div class="row justify-content-center mb-3">
-                <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
-                    <i class="bi bi-controller {{ $icon_size }}"></i>
-                </button>
-            </div>
-            <div class="row justify-content-center mb-3">
-                <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
-                    <i class="bi bi-chat-dots {{ $icon_size }}"></i>
-                </button>
-            </div>
-            <div class="row justify-content-center mb-3">
-                <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
-                    <i class="bi bi-list {{ $icon_size }}"></i>
-                </button>
-            </div>
+        <div class="col-3 d-sm-flex d-md-flex d-lg-none">
+            <nav class="d-flex flex-column">
+                <div class="d-flex flex-row my-3">
+                    <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
+                        <i class="bi bi-bar-chart {{ $icon_size }}"></i>
+                    </button>
+                    <span class="d-none d-md-block d-lg-none align-self-center ms-3">Ranking</span>
+                </div>
+                <div class="d-flex flex-row my-3">
+                    <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
+                        <i class="bi bi-controller {{ $icon_size }}"></i>
+                    </button>
+                    <span class="d-none d-md-block d-lg-none align-self-center ms-3">Games</span>
+                </div>
+                <div class="d-flex flex-row my-3">
+                    <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
+                        <i class="bi bi-chat-dots {{ $icon_size }}"></i>
+                    </button>
+                    <span class="d-none d-md-block d-lg-none align-self-center ms-3">Messages</span>
+                </div>
+                <div class="d-flex flex-row my-3">
+                    <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
+                        <i class="bi bi-list {{ $icon_size }}"></i>
+                    </button>
+                    <span class="d-none d-md-block d-lg-none align-self-center ms-3">Options</span>
+                </div>
+                <div class="d-flex flex-row my-3">
+                    <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
+                        <i class="bi bi-plus-circle {{ $icon_size }}"></i>
+                    </button>
+                    <span class="d-none d-md-block d-lg-none align-self-center ms-3">Create</span>
+                </div>
+            </nav>
         </div>
+
         <div class="col-2 d-lg-block d-none align-self-center">
             <div class="row justify-content-center mb-3">
-                <p class="text-center"><b>Game</b></p>
+                <p class="text-center"><b>Games</b></p>
             </div>
             <div class="row justify-content-center mb-3 p-3" style="background-color: rgb(112, 2, 237); background-image: linear-gradient(to left, rgba(0,0,0,0), rgba(255,140,0,1));">
                 2000 UP
@@ -56,11 +69,11 @@ $icon_size = 'fs-3';
             </div>
         </div>
         <div class="col-8">
-            <div class="row justify-content-center pt-3">
+            <div class="d-flex justify-content-center pt-3">
                 @include('partials.content')
             </div>
-            <div class="row justify-content-center py-3">
-            <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
+            <div class="d-none d-lg-flex justify-content-center py-3">
+                <button type="button" class="btn btn-secondary" style="width: auto; height: auto;">
                     <i class="bi bi-plus-circle {{ $icon_size }}"></i>
                 </button>
             </div>
@@ -69,8 +82,8 @@ $icon_size = 'fs-3';
             <div class="row justify-content-center">
                 <p class="text-center"><b>Online</b></p>
             </div>
-            <div class="row justify-content-center mb-3 p-3">
-                <div class="smooth-scroll" style="height: 10em; overflow-y: auto;">
+            <div class="row justify-content-center mb-3 py-3">
+                <div style="height: 10em; overflow-y: auto;">
                     <table class="table table-hover table-dark">
                         <thead>
                             <tr>
