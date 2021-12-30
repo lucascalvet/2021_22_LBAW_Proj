1d28fabe3109948e93f0cb9b001dbc380f6b4b48
Sérgio da Gama <sergio.r.gama@gmail.com>
2021-12-30 09:22:51 +0000

    done initial view for mini posts

---
+++ webapp/resources/views/partials/mini_post.blade.php
@@ -0,0 +1,21 @@
+<div class="p-1 h-50 m-lg-4 m-md-2 ms-lg-1 me-lg-1 bg-secondary shadow rounded-3 d-none d-md-block">
+    <div class="d-flex text-light">
+        <div class="me-auto m-2">Usernam</div>
+        <div class="m-2">Location</div>
+    </div>
+
+    <div class="d-flex">
+        <div class="m-2">
+            <div class="">Likes</div>
+            <div class="">count</div>
+        </div>
+
+        <div class="w-100 m-2">
+            <div class="">Comm</div>
+            <div class="d-flex w-100">
+                <div class="me-auto">count</div>
+                <div class="text-light">Time</div>
+            </div>
+        </div>
+    </div>
+</div>
