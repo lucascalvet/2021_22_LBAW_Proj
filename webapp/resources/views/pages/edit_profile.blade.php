caa9fb17bcb29b851ff658a5642196adf3c3d934
Sérgio da Gama <sergio.r.gama@gmail.com>
2021-12-31 15:24:56 +0000

    small changes on profile and edit

--- webapp/resources/views/pages/edit_profile.blade.php
+++ webapp/resources/views/pages/edit_profile.blade.php
@@ -6,10 +6,10 @@
 
 @include('partials.navbar')
 
-<section class="bg-info" id="profile">
+<section style="background-color: #999999;" id="profile">
 
 <div class="container vh-100">
-      <h1 class="text-light pt-5">Editing Profile</h1>
+      <h1 class="text-light pt-5 fw-bold">Editing Profile</h1>
       <div class="row gx-5">
           <div class="col-6">
             <label for="input-edit-name" class="form-label ps-3 mt-2 mb-0 fw-bold">Name</label>
@@ -33,26 +33,26 @@
 
           <div class="col-6 text-center">
 
-            <h3 class="mt-3 mb-3">Profile picture</h3>
+            <h3 class="mt-3 mb-3 fw-bold">Profile picture</h3>
             <div class="d-flex justify-content-center">
               <div class="bg-white rounded-circle p-5" style="max-width: 7em; max-height: 7em;">
                 <i class="bi bi-images"></i>
               </div>
             </div>
 
-            <h3 class="mt-4 mb-3">Cover picture</h3>
+            <h3 class="mt-4 mb-3 fw-bold">Cover picture</h3>
             <div class="bg-white rounded-3 p-5">
               <i class="bi bi-images"></i>
             </div>
 
-            <h4 class="mt-4 mb-3">Interests</h4>
+            <h4 class="mt-4 mb-3 fw-bold">Interests</h4>
             <div>
               <i class="bi bi-plus-circle-fill"></i>
             </div>
 
           </div>
           <row class="d-flex justify-content-center mt-4">
-            <button type="submit" class="btn btn-primary w-25">Save</button>
+            <button type="submit" class="btn btn-secondary w-25">Save</button>
           </row>
       </div>
     </div>
