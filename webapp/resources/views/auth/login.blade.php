73f83326d5b11aa940953b31af1af6746041880b
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2021-12-31 11:37:56 +0000

    Fixed login/registration

--- webapp/resources/views/auth/login.blade.php
+++ webapp/resources/views/auth/login.blade.php
@@ -1,111 +1,140 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="vh-100 gradient-custom  bg-dark text-white">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div style="border-radius: 1rem;">
-          <div class="card-body p-5 text-center">
+  <section class="vh-100 bg-dark text-white overflow-auto">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div style="border-radius: 1rem;">
+            <div class="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
+              <div class="mt-md-4 pb-5">
 
-              <h1 class="fw-bold mb-2 display-1">Social UP</h1>
-              <h3 class="text-white-50 mb-5">Step it up</h3>
+                <h1 class="fw-bold mb-2 display-1">Social UP</h1>
+                <h3 class="text-white-50 mb-5 pb-5">Step it up</h3>
 
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg rounded-pill" placeholder="username"/>
-                <!-- <label class="form-label" for="typeEmailX">username</label> -->
-              </div>
+                <form method="POST" action="{{ route('login') }}">
+                  {{ csrf_field() }}
+                  <div class="form-outline form-white mb-4">
+                    <input type="email" id="email" name="email"
+                      class="form-control form-control-lg rounded-pill @if ($errors->has('email')) is-invalid @endif"
+                      value="{{ old('email') }}" placeholder="email" required autofocus>
+                    @if ($errors->has('email'))
+                      <span class="invalid-feedback">
+                        {{ $errors->first('email') }}
+                      </span>
+                    @endif
+                  </div>
 
-              <div class="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg rounded-pill" placeholder="password"/>
-                <!-- <label class="form-label" for="typePasswordX">password</label> -->
-              </div>
+                  <div class="form-outline form-white mb-4">
+                    <input type="password" id="password" name="password"
+                      class="form-control form-control-lg rounded-pill @if ($errors->has('password')) is-invalid @endif" placeholder="password"
+                      required>
+                    @if ($errors->has('password'))
+                      <span class="invalid-feedback">
+                        {{ $errors->first('password') }}
+                      </span>
+                    @endif
+                  </div>
 
-              <!-- <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p> -->
+                  <div class="form-check mb-4 p-0 d-flex justify-content-center">
+                    <input class="form-check-input mx-0" type="checkbox" name="remember" value="" id="flexCheckDefault"
+                      type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label ms-2" for="flexCheckDefault">
+                      Remember Me
+                    </label>
+                  </div>
 
-              <div class="d-flex justify-content-center">
-                <div class="d-grid gap-2">
-                  <button class="btn btn-outline-light btn-lg px-5" type="submit">Sign In</button>
-                  <br>
-                  <button class="btn btn-outline-light btn-lg px-5" type="button">Sign Up</button>
+                  <!-- <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p> -->
+
+                  <div class="d-flex justify-content-center">
+                    <button class="btn btn-outline-light btn-lg px-5" type="submit">&nbsp;Sign In&nbsp;</button>
+                  </div>
+                </form>
+                <br>
+                <div class="d-flex justify-content-center">
+                  <a class="btn btn-outline-light btn-lg px-5" href="{{ route('register') }}">Sign Up</a>
                 </div>
+
+                <div class="text-center mt-5 pt-1">
+                  <h5 class="mb-1">Coming soon...</h5>
+                  <div class="d-flex justify-content-center">
+                    {{-- <a href="#!" class="text-white"> --}}
+                    <i class="bi bi-google fs-2 text-muted"></i>
+                    {{-- </a> --}}
+                    {{-- <a href="#!" class="text-white ms-5"> --}}
+                    <i class="bi bi-apple fs-2 ms-5 text-muted"></i>
+                    {{-- </a> --}}
+                    {{-- <a href="#!" class="text-white ms-5"> --}}
+                    <i class="bi bi-github fs-2 ms-5 text-muted"></i>
+                    {{-- </a> --}}
+                  </div>
+                </div>
+
               </div>
 
-              <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                <a href="#!" class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
-                </svg></a>
-                <a href="#!" class="text-white ms-5"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
-                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z"/>
-                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z"/>
-                </svg></a>
-                <a href="#!" class="text-white ms-5"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                </svg></a>
+              <div>
+                <p class="mb-0">Forgot your password? <a href="#!" class="text-white-50 fw-bold">Recover it</a>
+                </p>
               </div>
 
             </div>
-
-            <div>
-              <p class="mb-0">Forgot your password? <a href="#!" class="text-white-50 fw-bold">Recover it</a></p>
-            </div>
-
           </div>
         </div>
       </div>
     </div>
-</div>
-<div class="position-absolute bottom-0 end-0 m-5">
-<a href="#!" class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-  </svg></a>
-</div>
-<div class="modal top fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-mdb-backdrop="true" data-mdb-keyboard="true">
-  <div class="modal-dialog modal-lg ">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">...</div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
-          Close
-        </button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    <div class="position-absolute bottom-0 end-0 m-5">
+      <a href="#!" class="text-white"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+          fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+        </svg></a>
+    </div>
+    <div class="modal top fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+      data-mdb-backdrop="true" data-mdb-keyboard="true">
+      <div class="modal-dialog modal-lg ">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-</section>
-<!--
-<h2>Step it up</h2>
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+  </section>
+  <!--
+                                                                                                      <h2>Step it up</h2>
+                                                                                                      <form method="POST" action="{{ route('login') }}">
+                                                                                                          {{ csrf_field() }}
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
+                                                                                                          <label for="email">E-mail</label>
+                                                                                                          <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                                                                                                          @if ($errors->has('email'))
+                                                                                                              <span class="error">
+                                                                                                                {{ $errors->first('email') }}
+                                                                                                              </span>
+                                                                                                          @endif
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
+                                                                                                          <label for="password" >Password</label>
+                                                                                                          <input id="password" type="password" name="password" required>
+                                                                                                          @if ($errors->has('password'))
+                                                                                                              <span class="error">
+                                                                                                                  {{ $errors->first('password') }}
+                                                                                                              </span>
+                                                                                                          @endif
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
+                                                                                                          <label>
+                                                                                                              <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                                                                                          </label>
 
-    <button type="submit">Sign In</button>
-    <a class="button button-outline" href="{{ route('register') }}">Sign Up</a>
-</form>-->
+                                                                                                          <button type="submit">Sign In</button>
+                                                                                                          <a class="button button-outline" href="{{ route('register') }}">Sign Up</a>
+                                                                                                      </form>-->
 @endsection
