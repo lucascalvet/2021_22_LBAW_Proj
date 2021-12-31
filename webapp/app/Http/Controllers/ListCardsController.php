8099946f984b2efb5bc825406a191caee661da8e
frenato00 <up201908044@up.pt>
2021-12-31 11:21:20 +0000

    partial

--- webapp/app/Http/Controllers/ListCardsController.php
+++ webapp/app/Http/Controllers/ListCardsController.php
@@ -9,6 +9,6 @@ class ListCardsController extends Controller
     //
     public function show()
     {
-      return view('pages.listCards',['username'=>'John Doe', 'description'=>'Studied at FEUP, currently working on fixing is life.', 'comment'=>'Son of a gun','days_ago'=>'yesterday']);
+      return view('partials.listCards',['username'=>'John Doe', 'description'=>'Studied at FEUP, currently working on fixing is life.', 'comment'=>'Son of a gun','days_ago'=>'yesterday']);
     }
 }
